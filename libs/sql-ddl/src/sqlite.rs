e1434cfe30b1a7fb7c7c715c058510bd315058d0
Tom Houlé <tom@tomhoule.com>
2020-11-17 14:55:08 +0100

    wip

--- libs/sql-ddl/src/sqlite.rs
+++ libs/sql-ddl/src/sqlite.rs
@@ -1,4 +1,5 @@
-use std::fmt::Display;
+use crate::common::{IteratorJoin, SQL_INDENTATION};
+use std::fmt::{Display, Write as _};
 
 pub struct CreateTable<T> {
     table_name: T,
@@ -25,24 +26,40 @@ impl<T: Display> CreateTable<T> {
         }
     }
 
-    pub fn with_columns<F: FnOnce(SqliteColumns) -> SqliteColumns>(mut self, columns: F) -> Self {
-        self.columns = columns(SqliteColumns(String::new())).0;
+    pub fn with_columns<I: IntoIterator<Item = SqliteColumn<T>>>(mut self, columns: I) -> Self {
+        self.columns = columns.into_iter().join(",\n");
 
         self
     }
 }
 
-pub struct SqliteColumns(String);
-
-impl SqliteColumns {
-    pub fn column<T>(self, name: T, r#type: T, column_options: SqliteColumnOptions) -> Self {
-        self
-    }
-}
-
-#[derive(Debug, Default)]
-pub struct SqliteColumnOptions {
+pub struct SqliteColumn<T> {
+    name: T,
+    r#type: T,
     not_null: bool,
-    on_delete: Option<()>,
-    check: Option<()>,
+    primary_key: bool,
+}
+
+impl<T: Display> Display for SqliteColumn<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{indentation}{name} {tpe}{not_null}{primary_key}",
+            indentation = SQL_INDENTATION,
+            name = self.name,
+            tpe = self.r#type,
+            not_null = if self.not_null { " NOT NULL" } else { "" },
+            primary_key = if self.primary_key { " PRIMARY KEY" } else { "" },
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_create_table() {
+        todo!()
+    }
 }
